39ebf5774c4e6042fde8cc13c3adf4a957a135f7
onur <onurkucukkece@gmail.com>
2017-12-24 00:53:54 +0300

    guardian plugs and scopes in routes

--- lib/dand_web/router.ex
+++ lib/dand_web/router.ex
@@ -7,8 +7,15 @@ defmodule DandWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
   end
 
+  pipeline :browser_auth do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.EnsureAuthenticated, handler: DandWeb.Token
+    plug Guardian.Plug.LoadResource
+  end
   pipeline :api do
     plug :accepts, ["json"]
   end
