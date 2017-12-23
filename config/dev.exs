e89d14d032c54264ced56af3476ffbb247108596
onur <onurkucukkece@gmail.com>
2017-12-23 23:07:41 +0300

    changed some views to slim

--- config/dev.exs
+++ config/dev.exs
@@ -37,7 +37,7 @@ config :dand, DandWeb.Endpoint,
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{lib/dand_web/views/.*(ex)$},
-      ~r{lib/dand_web/templates/.*(eex)$}
+      ~r{lib/dand_web/templates/.*(eex|slim|slime)$}
     ]
   ]
 
