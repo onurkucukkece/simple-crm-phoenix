b805114a107edc46b1814c42d03c22c2798e9296
onur <onurkucukkece@gmail.com>
2017-12-24 23:35:48 +0300

    updated pages controller

--- lib/dand_web/controllers/page_controller.ex
+++ lib/dand_web/controllers/page_controller.ex
@@ -1,7 +1,48 @@
 defmodule DandWeb.PageController do
   use DandWeb, :controller
 
+	alias Dand.Accounts
+  alias Dand.Accounts.User
+  alias Dand.Accounts.Guardian
+
   def index(conn, _params) do
-    render conn, "index.html"
+    changeset = Accounts.change_user(%User{})
+    maybe_user = Guardian.Plug.current_resource(conn)
+    message = if maybe_user != nil do
+      "Someone is logged in"
+    else
+      "No one is logged in"
+    end
+    conn
+      |> put_flash(:info, message)
+      |> render("index.html", changeset: changeset, action: page_path(conn, :login), maybe_user: maybe_user)
+  end
+
+  def login(conn, %{"user" => %{"email" => email, "password" => password}}) do
+    Accounts.authenticate_user(email, password)
+    |> login_reply(conn)
+  end
+
+  defp login_reply({:error, error}, conn) do
+    conn
+    |> put_flash(:error, error)
+    |> redirect(to: "/")
+  end
+
+  defp login_reply({:ok, user}, conn) do
+    conn
+    |> put_flash(:success, "Welcome back!")
+    |> Guardian.Plug.sign_in(user)
+    |> redirect(to: "/")
+  end
+
+  def logout(conn, _) do
+    conn
+    |> Guardian.Plug.sign_out()
+    |> redirect(to: page_path(conn, :login))
+  end
+
+  def secret(conn, _params) do
+    render(conn, "secret.html")
   end
 end
