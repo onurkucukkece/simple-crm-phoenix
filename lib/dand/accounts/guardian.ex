aa3024a17e3524a0f8d70dec4fdfb19e59acbe7d
onur <onurkucukkece@gmail.com>
2017-12-24 23:36:17 +0300

    added other stuff

---
+++ lib/dand/accounts/guardian.ex
@@ -0,0 +1,16 @@
+defmodule Dand.Accounts.Guardian do
+  use Guardian, otp_app: :auth_ex
+
+  alias Dand.Accounts
+
+  def subject_for_token(user, _claims) do
+    {:ok, to_string(user.id)}
+  end
+
+  def resource_from_claims(claims) do
+    user = claims["sub"]
+    |> Accounts.get_user!
+    {:ok, user}
+    # If something goes wrong here return {:error, reason}
+  end
+end
