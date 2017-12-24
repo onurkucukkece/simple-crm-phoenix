3592d1bef1df0169af7274a064fb5b435206e327
onur <onurkucukkece@gmail.com>
2017-12-24 23:36:51 +0300

    updated accounts model

--- lib/dand/accounts/accounts.ex
+++ lib/dand/accounts/accounts.ex
@@ -8,6 +8,22 @@ defmodule Dand.Accounts do
 
   alias Dand.Accounts.User
 
+  alias Comeonin.Bcrypt
+
+  def authenticate_user(email, plain_text_password) do
+    query = from u in User, where: u.email == ^email
+    Repo.one(query)
+    |> check_password(plain_text_password)
+  end
+
+  defp check_password(nil, _), do: {:error, "Incorrect email or password"}
+  
+  defp check_password(user, plain_text_password) do
+    case Bcrypt.checkpw(plain_text_password, user.password) do
+      true -> {:ok, user}
+      false -> {:error, "Incorrect email or password"}
+    end
+  end
   @doc """
   Returns the list of users.
 
