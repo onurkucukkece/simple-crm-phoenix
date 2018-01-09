8e463f98f9c1cbb65dfebbc65cd50337eacd4f07
onur <onurkucukkece@gmail.com>
2018-01-09 23:57:42 +0300

    boyscout

--- lib/dand/accounts/pipeline.ex
+++ lib/dand/accounts/pipeline.ex
@@ -3,10 +3,9 @@ defmodule Dand.Accounts.Pipeline do
     otp_app: :dand,
     error_handler: Dand.Accounts.ErrorHandler,
     module: Dand.Accounts.Guardian
-  # If there is a session token, validate it
+
   plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
-  # If there is an authorization header, validate it
   plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
-  # Load the user if either of the verifications worked
   plug Guardian.Plug.LoadResource, allow_blank: true
+  plug Guardian.Plug.EnsureAuthenticated
 end
